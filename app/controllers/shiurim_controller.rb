a997f9e1d97c3d415f69e37e543b7f6b5a468212
Avraham Appel <avraham.appel@humi.ca>
2022-03-30 14:52:18 -0400

    Show page and links

--- app/controllers/shiurim_controller.rb
+++ app/controllers/shiurim_controller.rb
@@ -4,4 +4,8 @@ class ShiurimController < ApplicationController
   def index
     @shiurim = Shiur.order(:importance).all
   end
+
+  def show
+    @shiur = Shiur.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/shiurim' => 'shiurim#index'
+  get "shiurim" => "shiurim#index"
+  get "shiurim/:id" => "shiurim#show", as: "shiur"
 end
