c349846ab160d7c1eca2b30112790669c0cef794
Avraham Appel <avraham.appel@humi.ca>
2022-04-05 18:13:23 -0400

    Start adding tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it shouldn't save a user without a name" do
+    user = User.new email: "user@example.com"
+    assert_not user.save, "User saved without name"
+  end
 end
