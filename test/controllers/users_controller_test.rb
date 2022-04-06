4e6215cba6dc400bc77acdb6e9e4a88d3a918cf9
Avraham Appel <avraham.appel@humi.ca>
2022-04-06 12:05:59 -0400

    Create user account test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can sign up for an account" do
+    post users_url, params: {
+      user: {
+        name: "Jack",
+        email: "jack@beanstalk.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+    }
+
+    user_query = User.where(name: "Jack", email: "jack@beanstalk.com")
+    assert user_query.any?, "User was not created"
+    assert_equal user_query[0].id, session[:user_id], "User was not logged in"
+  end
 end
